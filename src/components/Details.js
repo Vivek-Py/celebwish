7c2ac5aa4a48a8bbf517822ee2efc05da2c6b168
Vivek <vivekpy@outlook.com>
2021-07-24 16:46:50 +0530

    Minor: Clean Template

---
+++ src/components/Details.js
@@ -0,0 +1,2 @@
+// Utilise this component to show more details as user click on a card/celeb
+// You can also ask user to enter the name
---
+++ src/components/Homepage.js
@@ -0,0 +1,2 @@
+// Create a page for celeb list, just keep 4-5 cards for now
+// Utilise dummy images for the same, just to see how it looks
